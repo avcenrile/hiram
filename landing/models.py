8961007042cdc67a7a16b679719eead1769956ed
Enri <avcenrile@users.noreply.github.com>
2024-07-03 13:16:32 +0800

    Finished the initial landing page and tweaked the designs to something closer to the final

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=30)
+    first_name=models.CharField(max_length=70)
+    last_name=models.CharField(max_length=50)
